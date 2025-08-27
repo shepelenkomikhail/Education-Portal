434aa354059898ad49c77233ad560bc049298709
Mykhailo Shepelenko <shepelenkomykhailo@gmail.com>
2025-08-27 13:59:02 +0200

    Course Controller, Model, View templates are added

---
+++ EducationPortal.App/EducationPortal.WebMVC/Models/CourseModel.cs
@@ -0,0 +1,8 @@
+namespace WebMVC.Models;
+
+public class CourseModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
---
+++ EducationPortal.App/EducationPortal.WebMVC/Models/MaterialModel.cs
@@ -0,0 +1,6 @@
+namespace WebMVC.Models;
+
+public class MaterialModel
+{
+    
+}
