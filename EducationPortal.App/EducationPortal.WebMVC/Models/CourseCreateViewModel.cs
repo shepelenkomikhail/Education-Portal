24c5b6858ed58a88994db343e0db425c2ab8effd
Mykhailo Shepelenko <shepelenkomykhailo@gmail.com>
2025-08-31 19:37:09 +0200

    Remove unnecessary Models; Add validation attributes to CourseCreateViewModel;

--- EducationPortal.App/EducationPortal.WebMVC/Models/CourseCreateViewModel.cs
+++ EducationPortal.App/EducationPortal.WebMVC/Models/CourseCreateViewModel.cs
@@ -1,13 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using WebMVC.Models;
 
 namespace EducationPortal.WebMVC.Models;
 
 public class CourseCreateViewModel
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    [Required(ErrorMessage = "Course name is required")]
+    [StringLength(50, ErrorMessage = "Course name cannot exceed 50 characters")]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Course description is required")]
+    [StringLength(3000, ErrorMessage = "Course description cannot exceed 3000 characters")]
+    public string Description { get; set; } = string.Empty;
+    
     public List<SkillModel> Skills { get; set; } = new();
     public List<MaterialModel> Materials { get; set; } = new();
+    
+    [Required(ErrorMessage = "Please select at least one skill")]
     public List<int> SelectedSkillIds { get; set; } = new();
+    
+    [Required(ErrorMessage = "Please select at least one material")]
     public List<int> SelectedMaterialIds { get; set; } = new();
 }
--- EducationPortal.App/EducationPortal.WebMVC/Models/VideoModel.cs
+++
@@ -1,6 +0,0 @@
-namespace WebMVC.Models;
-
-public class VideoModel
-{
-    
-}
