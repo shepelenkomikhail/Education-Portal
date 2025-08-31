b4122cde2e5a20ffb4c2e4b3ff6c795ed478130d
Mykhailo Shepelenko <shepelenkomykhailo@gmail.com>
2025-08-31 19:35:35 +0200

    Implement Add Course functionality on repository layer

--- EducationPortal.App/EducationPortal.Repo/Repositories/CourseRepository.cs
+++ EducationPortal.App/EducationPortal.Repo/Repositories/CourseRepository.cs
@@ -1,5 +1,6 @@
 using EducationPortal.Data.Models;
 using EducationPortal.Data.Repo.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace EducationPortal.Data.Repo.Repositories;
 
@@ -7,6 +8,37 @@ public class CourseRepository : BaseRepository<Course>, ICourseRepository
 {
     public CourseRepository(PortalDbContext context) : base(context)
     {
+    }
+    
+    public Course? GetByName(string name)
+    {
+        return context.Courses.FirstOrDefault(c => c.Name == name);
+    }
+
+    public bool AddSkillToCourse(int courseId, int skillId)
+    {
+        var course = context.Courses.Include(c => c.Skills)
+            .FirstOrDefault(c => c.Id == courseId);
+        var skill = context.Skills.Find(skillId);
+
+        if (course == null || skill == null)
+            return false;
+
+        course.Skills.Add(skill);
+        return true;
+    }
+
+    public bool AddMaterialToCourse(int courseId, int materialId)
+    {
+        var course = context.Courses.Include(c => c.Materials)
+            .FirstOrDefault(c => c.Id == courseId);
+        var material = context.Materials.Find(materialId);
+
+        if (course == null || material == null)
+            return false;
+
+        course.Materials.Add(material);
+        return true;
     }
     // TODO: Add repository methods specific to this repository
 }
--- EducationPortal.App/EducationPortal.Repo/RepositoryInterfaces/ICourseRepository.cs
+++ EducationPortal.App/EducationPortal.Repo/RepositoryInterfaces/ICourseRepository.cs
@@ -4,5 +4,7 @@ namespace EducationPortal.Data.Repo.RepositoryInterfaces;
 
 public interface ICourseRepository: IBaseRepository<Course>
 {
+    bool AddSkillToCourse(int courseId, int skillId);
+    bool AddMaterialToCourse(int courseId, int materialId);
     // TODO: Add repository signatures specific to this repository
 }
