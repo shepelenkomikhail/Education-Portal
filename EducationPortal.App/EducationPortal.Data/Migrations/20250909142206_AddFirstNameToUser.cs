aa217f1e1969a368e3f4e0a331f84b3ec2590286
Mykhailo Shepelenko <shepelenkomykhailo@gmail.com>
2025-09-11 19:44:15 +0200

    Recreate migrations and db

---
+++ EducationPortal.App/EducationPortal.Data/Migrations/20250909142206_AddFirstNameToUser.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducationPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstNameToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "FirstName" },
+                values: new object[] { "a4f1b2de-5634-4db9-970d-dfd8e0a506f3", "" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "FirstName" },
+                values: new object[] { "d503c3e4-412b-4105-94a6-a6a2d754b302", "" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "FirstName" },
+                values: new object[] { "81b8c1da-58a0-4656-ac8d-087530eb875f", "" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "ConcurrencyStamp", "FirstName" },
+                values: new object[] { "a1599d61-bf5f-42c8-9a09-ad4c4bf4fa34", "" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "ConcurrencyStamp", "FirstName" },
+                values: new object[] { "b065188f-ddab-48ab-9606-a555f0446591", "" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "a649e921-fd65-4a05-b254-506dafc0c58f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "a6e3eddf-e78a-4981-9be5-0bad29aab542");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "ff9c4c1d-e841-481e-b9a8-a3c85281b686");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "62a38f32-83b3-4216-95e4-68cffb9e9691");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "a8fb17d3-17ae-45af-bf80-38fac26b02ec");
+        }
+    }
+}
---
+++ EducationPortal.App/EducationPortal.Data/Migrations/20250910173121_AddRolesAndUserRoles.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EducationPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesAndUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { 1, "0a6fb46f-a61a-484f-8d20-20cdc8b42509", "Admin", "ADMIN" },
+                    { 2, "4461b7a6-7377-4b53-957e-474b4799a21b", "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "125c0735-0450-46a3-9055-86dfc3b69192");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "c2ec0f0a-48db-4968-9902-3b61493590e0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "c535adc0-4ce0-45be-a36e-881fb0ea9ef9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "7c412716-6d4e-423f-a97d-7d961418b3b0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "3e4c8546-ad6c-4eea-8fa0-f48dc7ba429f");
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 17, 31, 21, 75, DateTimeKind.Utc).AddTicks(9790));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 17, 31, 21, 75, DateTimeKind.Utc).AddTicks(9790));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 17, 31, 21, 75, DateTimeKind.Utc).AddTicks(9790));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 17, 31, 21, 75, DateTimeKind.Utc).AddTicks(9800));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 17, 31, 21, 75, DateTimeKind.Utc).AddTicks(9800));
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 2, 4 },
+                    { 2, 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "5ab6b9d1-5dcc-4dac-bc07-7fb1899512f9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d3477f51-aaea-4e79-b698-43d180016cbb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "f2fec7a1-ef31-45d2-94ec-38d01f134b3d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "b2169f94-dbe3-4ab8-bab7-b360a837c77b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "2e6f5cda-4121-4a5a-812a-57d46f2ba520");
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 16, 51, 16, 312, DateTimeKind.Utc).AddTicks(3190));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 16, 51, 16, 312, DateTimeKind.Utc).AddTicks(3190));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 16, 51, 16, 312, DateTimeKind.Utc).AddTicks(3190));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 16, 51, 16, 312, DateTimeKind.Utc).AddTicks(3190));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 10, 16, 51, 16, 312, DateTimeKind.Utc).AddTicks(3190));
+        }
+    }
+}
